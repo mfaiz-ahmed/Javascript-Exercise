79f2af71353989a76b0be21594969e01d3650e01
faizmaqsood668@gmail.com <faizmaqsood668@gmail.com>
2024-03-26 03:29:42 +0500

    exercise

--- Maths-Date&Time Questions/app.js
+++ Maths-Date&Time Questions/app.js
@@ -56,3 +56,113 @@
 
 // Q.5
 
+
+// const input = document.querySelector('.input');
+// const absolute = document.querySelector('.absolute');
+
+// const number = +prompt('Enter a Number');
+
+// input.innerHTML = number;
+
+// absolute.innerHTML =  Math.abs(number);
+
+
+// Q.6
+
+
+
+// const num = Math.random()*6;
+// const dice = document.querySelector('.dice')
+
+// dice.innerHTML = Math.ceil(num);
+
+// Q.7
+
+
+// const num = Math.random()*2;
+
+// const coin = Math.ceil(num);
+// const toss = document.querySelector('.toss')
+
+
+// if(coin === 1){
+//     toss.innerHTML = `${coin} Heads`;
+// }
+// else{
+//     toss.innerHTML = `${coin} Tails`;
+// }
+
+
+// Q.8
+
+// const number = Math.random()*100;
+// const random = document.querySelector('.random');
+
+// random.innerHTML = Math.ceil(number);
+
+
+// Q.9
+
+// const userInput = prompt("Enter your Body Weight");
+// const weight = document.querySelector('.weight');
+
+
+// weight.innerHTML = `The weight of the user is ${userInput} kilograms`;
+
+// Q.10
+
+
+// const secretNumber = 7;
+
+// const userInput = +prompt('Enter a number between 1 and 10');
+// const result = document.querySelector('.result');
+
+// if(userInput === secretNumber){
+//     result.innerHTML = "Congratulations! You won";
+// }else{
+//     result.innerHTML = "Try Again!";
+// }
+
+// Q.11
+
+// const time = document.querySelector('.time');
+
+// time.innerHTML = new Date();
+
+
+// Q.12
+
+// const month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+
+// const monthSpan = document.querySelector('.month-span');
+
+// monthSpan.innerHTML = month[new Date().getMonth()];
+
+
+// Q.13
+
+
+
+// const day = ["Sun" , "Mon" , "Tue" , "Wed" , "Thur" , "Fri" , "Sat"];
+
+// const daySpan = document.querySelector('.day-span');
+
+// daySpan.innerHTML = day[new Date().getDay()];
+
+// Q.14
+
+// const day = ["Sun" , "Mon" , "Tue" , "Wed" , "Thur" , "Fri" , "Sat"];
+
+// const daySpan = document.querySelector('.daySpan');
+
+// const days =  day[new Date().getDay()];
+
+// if(days === "Sat" || days === "Sun"){
+//     daySpan.innerHTML = "It's Fun day";
+// }else{
+//     daySpan.innerHTML = "It's Working day"
+// }
+
+
+// Q.16
+
